b483362aec7995e7806665ca39756f56642e109f
vcxz0330 <vcxz0330@gmail.com>
2025-09-26 15:44:31 +0900

    feat: Update User.java, SsafitUserRepository.java, SsafitUserRepositoryImpl.java

--- src/main/java/ssafit/model/dto/User.java
+++ src/main/java/ssafit/model/dto/User.java
@@ -1,5 +1,59 @@
 package ssafit.model.dto;
 
-public class User {
+import java.io.Serializable;
 
+public class User implements Serializable {
+	private String userId;
+	private String password;
+	private String name;
+	private String email;
+	
+	public User() {
+		
+	}
+
+	public User(String userId, String password, String name, String email) {
+		this.userId = userId;
+		this.password = password;
+		this.name = name;
+		this.email = email;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
+	}
+	
 }
